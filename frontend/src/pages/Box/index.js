5f460bc900b0f3dc9225b04e24f312a96fd78be6
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-09 09:29:06 -0300

    Files Upload using click and dropzone

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -5,6 +5,8 @@ import { formatDistance } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
 import { MdInsertDriveFile } from 'react-icons/md';
+import Dropzone from 'react-dropzone';
+
 import logo from '../../assets/logo.svg'
 
 export default function Box({ match }) {
@@ -18,15 +20,34 @@ export default function Box({ match }) {
         }
         getBox();
     }, [match.params.id]);
+
+    function handleUpload (files){
+        files.forEach(file=>{
+            const data = new FormData();
+            const box = match.params.id;
+            data.append('file', file);
+            api.post(`boxes/${box}/files`, data)
+        })
+    }
     return (
         <div id="box-container">
             <header>
                 <img src={logo} alt="logo" />
                 <h1>{box.title}</h1>
             </header>
+
+            <Dropzone onDropAccepted={handleUpload}>
+                {({getRootProps, getInputProps})=>(
+                    <div className="upload" { ...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <p>Arraste arquivos ou clique aqui</p>
+                    </div>
+                )}
+            </Dropzone>
+
             <ul>
                 {box.files && box.files.map(file => (
-                    <li>
+                    <li key={file._id}>
                         <a className="fileInfo"
                             href={file.url}
                             target="_blank"
