0bd518a98482c30fc4af045100e57dc479a19d84
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-08 23:58:18 -0300

    Create Design Box page

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -1,16 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.css';
-import api from '../../services/api';
-
+// import api from '../../services/api';
+import { MdInsertDriveFile } from 'react-icons/md';
 import logo from '../../assets/logo.svg'
 
-export default function Box(){
+export default function Box() {
     const [newBox, setNewBox] = useState('');
 
-
     return (
-        <div id="main-container">
-            
+        <div id="box-container">
+            <header>
+                <img src={logo} alt="logo" />
+                <h1>RocketSeat</h1>
+            </header>
+            <ul>
+                <li>
+                    <a className="fileInfo" href="#">
+                        <MdInsertDriveFile size={24} color="#a5cfff" />
+                        <strong>Desafio.pdf</strong>
+                    </a>
+                    <span>Há 5 minutos atrás</span>
+                </li>
+                <li>
+                    <a className="fileInfo" href="#">
+                        <MdInsertDriveFile size={24} color="#a5cfff" />
+                        <strong>Desafio.pdf</strong>
+                    </a>
+                    <span>Há 5 minutos atrás</span>
+                </li>
+            </ul>
+
         </div>
     );
 }
