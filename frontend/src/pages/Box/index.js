d908ee3cbb31c6b694abb1884f86f8884232126c
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-09 09:09:59 -0300

    List files of box and apply file creation time

--- frontend/src/pages/Box/index.js
+++ frontend/src/pages/Box/index.js
@@ -1,33 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
-// import api from '../../services/api';
+import api from '../../services/api';
+import { formatDistance } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
+import pt from 'date-fns/locale/pt';
 import { MdInsertDriveFile } from 'react-icons/md';
 import logo from '../../assets/logo.svg'
 
-export default function Box() {
-    const [newBox, setNewBox] = useState('');
+export default function Box({ match }) {
+    const [box, setBox] = useState({});
 
+    useEffect(() => {
+        async function getBox() {
+            const boxId = match.params.id
+            const response = await api.get(`/boxes/${boxId}`);
+            setBox(response.data);
+        }
+        getBox();
+    }, [match.params.id]);
     return (
         <div id="box-container">
             <header>
                 <img src={logo} alt="logo" />
-                <h1>RocketSeat</h1>
+                <h1>{box.title}</h1>
             </header>
             <ul>
-                <li>
-                    <a className="fileInfo" href="#">
-                        <MdInsertDriveFile size={24} color="#a5cfff" />
-                        <strong>Desafio.pdf</strong>
-                    </a>
-                    <span>Há 5 minutos atrás</span>
-                </li>
-                <li>
-                    <a className="fileInfo" href="#">
-                        <MdInsertDriveFile size={24} color="#a5cfff" />
-                        <strong>Desafio.pdf</strong>
-                    </a>
-                    <span>Há 5 minutos atrás</span>
-                </li>
+                {box.files && box.files.map(file => (
+                    <li>
+                        <a className="fileInfo"
+                            href={file.url}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <MdInsertDriveFile size={24} color="#a5cfff" />
+                            <strong>{file.title}</strong>
+                        </a>
+                        {/* <span>
+                            {file.createdAt}
+                        </span> */}
+                        <span>
+                            há{" "}
+                            {formatDistance(
+                                utcToZonedTime(file.createdAt, 'America/Sao_Paulo'), 
+                                new Date(), {
+                                    locale: pt
+                             })
+                            }
+                        </span>
+                    </li>
+                ))}
+
+
             </ul>
 
         </div>
