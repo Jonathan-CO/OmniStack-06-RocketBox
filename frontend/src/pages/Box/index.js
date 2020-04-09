21ccc4c9bc0f7597e290d9f1bf0f147ecac8a05a
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-08 23:48:04 -0300

    create Box login and with createBox and redirect to Box created

---
+++ frontend/src/pages/Box/index.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './style.css';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg'
+
+export default function Box(){
+    const [newBox, setNewBox] = useState('');
+
+
+    return (
+        <div id="main-container">
+            
+        </div>
+    );
+}
--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
 import './style.css';
+import api from '../../services/api';
+
+
+import logo from '../../assets/logo.svg'
 
 export default function Main(){
+    const [newBox, setNewBox] = useState('');
+    const history = useHistory();
+
+
+    async function handleSumit(e){
+        e.preventDefault();
+        const response = await api.post('/boxes', { 
+            title: newBox 
+        })
+        history.push(`/box/${response.data._id}`);
+        // console.log(response.data);
+    }
+
     return (
         <div id="main-container">
-            <form >
-                {/* <img src={} alt=""/> */}
-                <input placeholder="Criar um box"/>
+            <form onSubmit={handleSumit}>
+                <img src={logo} alt="logo"/>
+                <input 
+                value={newBox}
+                onChange={e => setNewBox(e.target.value)}
+                placeholder="Criar um box"/>
                 <button type="submit">Criar</button>
             </form>
         </div>
