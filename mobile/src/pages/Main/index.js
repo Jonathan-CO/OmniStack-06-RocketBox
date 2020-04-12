d13d5acf577108aeb3af451a9dea475a7e684a99
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-11 23:54:24 -0300

    create design screen Main

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+import logo from '../../assets/logo.svg';
 
 export default function Main() {
     return (
         <View style={{ backgroundColor: "red" }}>
-            <Text>Hello OmnisTack 06</Text>
+            
+            <Image style={styles.logo} source={logo}/>
+            
+            <TextInput 
+            style={StyleSheet.input}
+            placeholder="Crie um box"
+            placeholderTextColor="#999"
+            autoCorrect={false}
+            underlineColorAndroid="transparent"
+            />
+    <TouchableOpacity
+    onPress={()=>{}}
+    style={styles.button}>
+        <Text style={StyleSheet.buttonText}>Criar</Text>
+    </TouchableOpacity>
         </View>
     )
 }
